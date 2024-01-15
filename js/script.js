8d6e83b232b7464e20c34ed86b0b2c9b9a4a7b7a
Passarelli / Francesco Passarelli <passarellif72@gmail.com>
2024-01-15 16:16:34 +0100

    milestone 3 finita

---
+++ js/script.js
@@ -0,0 +1,252 @@
+// Estraggo la funzione createApp dall'oggetto vue
+const { createApp } = Vue;
+
+//  Creo l'istanza di Vue
+createApp ({
+    data() {
+        return {
+            searchedUser: '',
+            activeContact: 0,
+            newMessage: '',
+            contacts: [
+                {
+                    name: 'Michele',
+                    avatar: './img/avatar_1.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        },
+                    ],
+                },
+                {
+                    name: 'Fabio',
+                    avatar: './img/avatar_2.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Samuele',
+                    avatar: './img/avatar_3.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro B.',
+                    avatar: './img/avatar_4.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro L.',
+                    avatar: './img/avatar_5.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ricordati di chiamare la nonna',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Va bene, stasera la sento',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Claudia',
+                    avatar: './img/avatar_6.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao Claudia, hai novità?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Non ancora',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'Nessuna nuova, buona nuova',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Federico',
+                    avatar: './img/avatar_7.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Davide',
+                    avatar: './img/avatar_8.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'OK!!',
+                            status: 'received'
+                        }
+                    ],
+                }
+            ]
+            
+        };
+        
+    },
+    methods: {
+        changeActiveContact(index) {
+            console.log('PRIMA', this.activeContact);
+            this.activeContact = index;
+            console.log('DOPO', this.activeContact);
+        },
+        addNewMessage() {
+            if (this.newMessage.length > 0) {
+                const newMessageObj = {         
+                    date: this.getTheMoment(),
+                    message:this.newMessage,
+                    status: 'sent'
+                };
+        
+                this
+                    .contacts
+                        [this.activeContact]
+                            .messages
+                                .push(newMessageObj);
+        
+                this.newMessage = '';
+        
+                setTimeout(() => {
+                    const newResponseMessageObj = {
+                        date: this.getTheMoment(),
+                        message: 'Ok',
+                        status: 'received'
+                    };
+            
+                    this
+                        .contacts
+                            [this.activeContact]
+                                .messages
+                                    .push(newResponseMessageObj);
+                }, 1000);
+            }
+        },
+        getTheMoment() {
+            let finalString ='';
+
+            const now = new Date();
+
+            finalString += now.getDate().toString().padStart(2, '0');
+            finalString += '/';
+            finalString += (now.getMonth() + 1).toString().padStart(2, '0');
+            finalString += '/';
+            finalString += now.getFullYear();
+
+            finalString += now.getHours().toString().padStart(2, '0');
+            finalString += ':';
+            finalString += now.getMinutes().toString().padStart(2, '0');
+            finalString += ':';
+            finalString += now.getSeconds().toString().padStart(2, '0');
+
+
+            return finalString;
+        },
+        searchContact() {
+            this.contacts.forEach((contact) => {
+                if (
+                   contact.name
+                      .toLowerCase()
+                      .includes(this.searchedUser.toLowerCase())
+                ) {
+                   contact.visible = true;
+                } else {
+                   contact.visible = false;
+                }
+            });
+            console.log(searchedUser);
+        },
+    }
+    // Monto l'istanza di Vue in pagina
+}).mount('#app');
+
